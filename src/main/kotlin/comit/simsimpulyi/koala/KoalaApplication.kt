ed6c0be6ddf51ffdcda9bddc3d15745213fb6c31
KimBeomJin <sdpthf@naver.com>
2022-07-13 17:14:34 +0900

    chore: init project

---
+++ src/main/kotlin/comit/simsimpulyi/koala/KoalaApplication.kt
@@ -0,0 +1,11 @@
+package comit.simsimpulyi.koala
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KoalaApplication
+
+fun main(args: Array<String>) {
+    runApplication<KoalaApplication>(*args)
+}
---
+++ src/test/kotlin/comit/simsimpulyi/koala/KoalaApplicationTests.kt
@@ -0,0 +1,13 @@
+package comit.simsimpulyi.koala
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KoalaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
