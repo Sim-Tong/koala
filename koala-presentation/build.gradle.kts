8632b13fbeabb472716510063a1a44cbe872a839
KimBeomJin <sdpthf@naver.com>
2022-07-22 00:13:34 +0900

    chore: presentation build.gradle.kts

--- koala-presentation/build.gradle.kts
+++ koala-presentation/build.gradle.kts
@@ -1,2 +1,18 @@
+plugins {
+    id("org.springframework.boot") version PluginVersions.SPRING_BOOT_VERSION
+    id("io.spring.dependency-management") version PluginVersions.DEPENDENCY_MANAGER_VERSION
+    kotlin("plugin.spring") version PluginVersions.SPRING_PLUGIN_VERSION
+}
+
 dependencies {
+    implementation(Dependencies.SPRING_VALIDATION)
+    implementation(Dependencies.SPRING_WEB)
+
+    implementation(Dependencies.JACKSON)
+
+    implementation(project(":koala-application"))
+}
+
+tasks.getByName<Jar>("jar") {
+    enabled = false
 }
