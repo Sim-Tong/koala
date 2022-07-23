17949cfbfa637b26f4130d1c17def2aa9c63bb47
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-07-23 20:36:25 +0900

    revert: dependencies cancel

--- koala-presentation/build.gradle.kts
+++ koala-presentation/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("org.springframework.boot") version PluginVersions.SPRING_BOOT_VERSION
     id("io.spring.dependency-management") version PluginVersions.DEPENDENCY_MANAGER_VERSION
     kotlin("plugin.spring") version PluginVersions.SPRING_PLUGIN_VERSION
-    kotlin("plugin.jpa") version PluginVersions.JPA_PLUGIN_VERSION
 }
 
 dependencies {
@@ -10,8 +9,6 @@ dependencies {
     implementation(Dependencies.SPRING_WEB)
 
     implementation(Dependencies.JACKSON)
-
-    implementation(Dependencies.MYSQL_CONNECTOR)
 
     implementation(project(":koala-application"))
 }
