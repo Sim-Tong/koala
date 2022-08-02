830cde54540e00366f9bcf28ac1f1b33d6d5cfb0
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-02 21:01:20 +0900

    refactor: BaseEntity reform

--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/BaseTimeEntity.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/BaseTimeEntity.kt
@@ -1,18 +1,11 @@
 package comit.simsimpulyi.koala.persistence
 
 import java.time.LocalDateTime
-import java.util.*
-import javax.persistence.Column
-import javax.persistence.Id
 import javax.persistence.MappedSuperclass
 import javax.validation.constraints.NotNull
 
 @MappedSuperclass
-abstract class BaseEntity (
+abstract class BaseTimeEntity(
     @field:NotNull
     val createDate: LocalDateTime = LocalDateTime.now(),
-
-    @Id
-    @Column(columnDefinition = "BINARY(16)")
-    val id: UUID = UUID.randomUUID()
 )
