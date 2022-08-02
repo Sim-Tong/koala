4d4dd05af6a0b9769b23dc82125862a3d0448172
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-02 21:00:18 +0900

    refactor: IdClass >>> EmbeddedId

--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/like/entity/LikeEntity.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/like/entity/LikeEntity.kt
@@ -1,27 +1,14 @@
 package comit.simsimpulyi.koala.persistence.like.entity
 
-import comit.simsimpulyi.koala.persistence.feed.entity.FeedEntity
-import comit.simsimpulyi.koala.persistence.user.entity.UserEntity
+import comit.simsimpulyi.koala.persistence.BaseTimeEntity
 import java.io.Serializable
-import java.time.LocalDateTime
-import javax.persistence.*
-import javax.validation.constraints.NotNull
+import javax.persistence.EmbeddedId
+import javax.persistence.Entity
+import javax.persistence.Table
 
 @Entity
-@IdClass(LikeEntityId::class)
-@Table(name = "tb_like")
+@Table(name = "tbl_like")
 class LikeEntity(
-
-    @Id
-    @ManyToOne
-    @JoinColumn(name = "user_id", columnDefinition = "BINARY(16)")
-    val user: UserEntity,
-
-    @Id
-    @ManyToOne
-    @JoinColumn(name = "feed_id", columnDefinition = "BINARY(16)")
-    val feed: FeedEntity,
-
-    @field:NotNull
-    val createDate: LocalDateTime = LocalDateTime.now()
-): Serializable
+    @EmbeddedId
+    val key: LikeEntityId
+): Serializable, BaseTimeEntity()
--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/like/entity/LikeEntityId.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/like/entity/LikeEntityId.kt
@@ -1,9 +1,20 @@
 package comit.simsimpulyi.koala.persistence.like.entity
 
+import comit.simsimpulyi.koala.persistence.feed.entity.FeedEntity
+import comit.simsimpulyi.koala.persistence.user.entity.UserEntity
 import java.io.Serializable
-import java.util.*
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.validation.constraints.NotNull
 
 data class LikeEntityId(
-    val user: UUID,
-    val feed: UUID
+    @field:NotNull
+    @ManyToOne
+    @JoinColumn(columnDefinition = "BINARY(16)")
+    val user: UserEntity,
+
+    @field:NotNull
+    @ManyToOne
+    @JoinColumn(columnDefinition = "BINARY(16)")
+    val feed: FeedEntity
 ): Serializable
