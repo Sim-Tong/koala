95e1b229d32f04ff3174f00f7efaa9455526f08c
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-05 01:29:41 +0900

    add: Entity Mapper

---
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/feed/mapper/FeedMapper.kt
@@ -0,0 +1,7 @@
+package comit.simsimpulyi.koala.persistence.feed.mapper
+
+import org.mapstruct.Mapper
+
+@Mapper(componentModel = "spring")
+class FeedMapper {
+}
---
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/user/mapper/UserMapper.kt
@@ -0,0 +1,8 @@
+package comit.simsimpulyi.koala.persistence.user.mapper
+
+import org.mapstruct.Mapper
+
+@Mapper(componentModel = "spring")
+class UserMapper {
+
+}
