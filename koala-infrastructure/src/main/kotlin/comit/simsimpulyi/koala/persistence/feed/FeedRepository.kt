a9af636ba45f05bd7e7fc69dcce5d54a5753a996
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-05 00:21:52 +0900

    refactor: Repository 어노테이션

--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/feed/FeedRepository.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/feed/FeedRepository.kt
@@ -2,7 +2,9 @@ package comit.simsimpulyi.koala.persistence.feed
 
 import comit.simsimpulyi.koala.persistence.feed.entity.FeedEntity
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 import java.util.*
 
+@Repository
 interface FeedRepository : CrudRepository<FeedEntity, UUID> {
 }
--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/user/UserRepository.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/user/UserRepository.kt
@@ -2,7 +2,9 @@ package comit.simsimpulyi.koala.persistence.user
 
 import comit.simsimpulyi.koala.persistence.user.entity.UserEntity
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 import java.util.*
 
+@Repository
 interface UserRepository : CrudRepository<UserEntity, UUID> {
 }
