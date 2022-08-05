504cd03187bf4d72942c4760e39395e12e177131
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-05 16:13:06 +0900

    refactor: Like Adapter

--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/like/LikePersistenceAdapter.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/like/LikePersistenceAdapter.kt
@@ -1,13 +1,30 @@
 package comit.simsimpulyi.koala.persistence.like
 
 import comit.simsimpulyi.koala.domain.like.model.Like
+import comit.simsimpulyi.koala.domain.like.spi.QueryLikePort
 import comit.simsimpulyi.koala.domain.like.spi.SaveLikePort
+import org.springframework.stereotype.Component
+import java.util.*
 
+@Component
 class LikePersistenceAdapter(
     private val likeRepository: LikeRepository
-) : SaveLikePort {
+) : SaveLikePort, QueryLikePort {
 
     override fun saveLike(like: Like): Like {
         TODO("Not yet implemented")
     }
+
+    override fun getLike(userId: UUID, feedId: UUID): Like {
+        TODO("Not yet implemented")
+    }
+
+    override fun getLikeAllByUser(userId: UUID): List<Like> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getLikeAllByFeed(feedId: UUID): List<Like> {
+        TODO("Not yet implemented")
+    }
+
 }
