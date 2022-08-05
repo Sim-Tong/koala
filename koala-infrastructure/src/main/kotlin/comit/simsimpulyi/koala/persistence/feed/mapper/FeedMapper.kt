198ce94a20529ee717c66cf1437403c104fb9909
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-05 15:20:47 +0900

    refactor: expression mapper

--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/feed/mapper/FeedMapper.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/feed/mapper/FeedMapper.kt
@@ -3,17 +3,17 @@ package comit.simsimpulyi.koala.persistence.feed.mapper
 import comit.simsimpulyi.koala.domain.feed.model.Feed
 import comit.simsimpulyi.koala.persistence.BaseMapper
 import comit.simsimpulyi.koala.persistence.feed.entity.FeedEntity
-import comit.simsimpulyi.koala.persistence.user.UserRepository
+import comit.simsimpulyi.koala.persistence.user.UserPersistenceAdapter
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
 
-@Mapper(componentModel = "spring", imports = [UserRepository::class])
+@Mapper(componentModel = "spring", imports = [UserPersistenceAdapter::class])
 interface FeedMapper : BaseMapper<FeedEntity, Feed> {
 
     @Mapping(target = "writerId", expression = "java(entity.getWriter().getId())")
     override fun toDomain(entity: FeedEntity): Feed
 
-    @Mapping(target = "writer", expression = "java(new UserRepository().queryUserEntityById(model.getWriterId()))")
+    @Mapping(target = "writer", expression = "java(new UserPersistenceAdapter().getUser(model.getWriterId()))")
     override fun toEntity(model: Feed): FeedEntity
 }
 
