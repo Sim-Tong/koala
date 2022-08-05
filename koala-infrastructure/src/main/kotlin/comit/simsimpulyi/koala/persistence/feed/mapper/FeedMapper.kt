8d11c8f8dae6a47b45105100d6023eda9fa74d0f
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-05 16:13:56 +0900

    refactor: FeedMapper DI 추가

--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/feed/mapper/FeedMapper.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/feed/mapper/FeedMapper.kt
@@ -6,14 +6,18 @@ import comit.simsimpulyi.koala.persistence.feed.entity.FeedEntity
 import comit.simsimpulyi.koala.persistence.user.UserPersistenceAdapter
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
+import org.springframework.beans.factory.annotation.Autowired
 
-@Mapper(componentModel = "spring", imports = [UserPersistenceAdapter::class])
-interface FeedMapper : BaseMapper<FeedEntity, Feed> {
+@Mapper(componentModel = "spring")
+abstract class FeedMapper : BaseMapper<FeedEntity, Feed> {
+
+    @Autowired
+    protected lateinit var userPersistenceAdapter: UserPersistenceAdapter
 
     @Mapping(target = "writerId", expression = "java(entity.getWriter().getId())")
-    override fun toDomain(entity: FeedEntity): Feed
+    abstract override fun toDomain(entity: FeedEntity): Feed
 
-    @Mapping(target = "writer", expression = "java(new UserPersistenceAdapter().getUser(model.getWriterId()))")
-    override fun toEntity(model: Feed): FeedEntity
+    @Mapping(target = "writer", expression = "java(userPersistenceAdapter().getUser(model.getWriterId()))")
+    abstract override fun toEntity(model: Feed): FeedEntity
 }
 
