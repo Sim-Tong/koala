6378f91b5e689a6ddc0c92bfd2182a5d30748d28
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-05 15:28:17 +0900

    refactor: Bean 등록

--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/feed/FeedPersistenceAdapter.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/feed/FeedPersistenceAdapter.kt
@@ -3,8 +3,10 @@ package comit.simsimpulyi.koala.persistence.feed
 import comit.simsimpulyi.koala.domain.feed.model.Feed
 import comit.simsimpulyi.koala.domain.feed.spi.QueryFeedPort
 import comit.simsimpulyi.koala.domain.feed.spi.SaveFeedPort
+import org.springframework.stereotype.Component
 import java.util.*
 
+@Component
 class FeedPersistenceAdapter(
     private val feedRepository: FeedRepository
 ) : SaveFeedPort, QueryFeedPort {
--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/user/UserPersistenceAdapter.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/user/UserPersistenceAdapter.kt
@@ -3,8 +3,10 @@ package comit.simsimpulyi.koala.persistence.user
 import comit.simsimpulyi.koala.domain.user.model.User
 import comit.simsimpulyi.koala.domain.user.spi.QueryUserPort
 import comit.simsimpulyi.koala.domain.user.spi.SaveUserPort
+import org.springframework.stereotype.Component
 import java.util.*
 
+@Component
 class UserPersistenceAdapter(
     private val repository: UserRepository
 ) : SaveUserPort, QueryUserPort {
