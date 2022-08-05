ff38bfc311bd7d68701b11d3638ca8dbfd63db2f
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-05 13:39:48 +0900

    refactor: FeedMapper Mapping

--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/feed/mapper/FeedMapper.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/feed/mapper/FeedMapper.kt
@@ -3,16 +3,17 @@ package comit.simsimpulyi.koala.persistence.feed.mapper
 import comit.simsimpulyi.koala.domain.feed.model.Feed
 import comit.simsimpulyi.koala.persistence.BaseMapper
 import comit.simsimpulyi.koala.persistence.feed.entity.FeedEntity
+import comit.simsimpulyi.koala.persistence.user.entity.UserEntity
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 
 @Mapper(componentModel = "spring")
 interface FeedMapper : BaseMapper<FeedEntity, Feed> {
 
-    override fun toDomain(entity: FeedEntity): Feed {
-        TODO("Not yet implemented")
-    }
+    @Mapping(target = "writerId", expression = "java(entity.getWriter().getId())")
+    override fun toDomain(entity: FeedEntity): Feed
 
-    override fun toEntity(model: Feed): FeedEntity {
-        TODO("Not yet implemented")
-    }
+    @Mapping(target = "writer", source = "writer")
+    fun toEntity(model: Feed, writer: UserEntity): FeedEntity
 }
+
