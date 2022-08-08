73c2d3aad54306f71fa60ae419fe8ab1d7a70dda
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-08 17:58:27 +0900

    build: Aws Dependency 추가

--- buildSrc/src/main/kotlin/DependencyVersions.kt
+++ buildSrc/src/main/kotlin/DependencyVersions.kt
@@ -1,5 +1,5 @@
 object DependencyVersions {
     const val JWT_VERSION = "0.9.1"
     const val MAPSTRUCT_VERSION = "1.5.2.Final"
-    const val AWS_VERSION = "1.12.276"
+    const val AWS_VERSION = "2.2.6.RELEASE"
 }
