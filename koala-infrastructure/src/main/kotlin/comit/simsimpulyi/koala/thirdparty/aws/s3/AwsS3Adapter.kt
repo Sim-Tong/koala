32ec0ea5cddc611914f52062a259a2b52cb59c45
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-08 18:03:05 +0900

    add: S3 Adapter

--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/thirdparty/aws/s3/AwsS3Adapter.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/thirdparty/aws/s3/AwsS3Adapter.kt
@@ -1,20 +1,27 @@
 package comit.simsimpulyi.koala.thirdparty.aws.s3
 
-import comit.simsimpulyi.koala.domain.file.spi.UploadFilePort
-import comit.simsimpulyi.koala.thirdparty.aws.s3.spi.SaveFilePort
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.CannedAccessControlList
+import com.amazonaws.services.s3.model.PutObjectRequest
+import comit.simsimpulyi.koala.domain.feed.model.BucketFolder
+import comit.simsimpulyi.koala.domain.feed.spi.UploadFilePort
 import org.springframework.stereotype.Component
+import java.io.File
+import java.util.*
 
 @Component
 class AwsS3Adapter(
-    private val saveFilePort: SaveFilePort,
+    private val amazonS3Client: AmazonS3Client,
+    private val awsS3Properties: AwsS3Properties
 ) : UploadFilePort {
-    override fun storeFile() {
-        TODO("Not yet implemented")
-    }
 
-    override fun uploadFile() {
-        TODO("Not yet implemented")
-    }
+    override fun uploadFile(file: File, folder: BucketFolder): String {
+        val fileKey = "${folder.name}/${UUID.randomUUID()}/${file.name}"
 
+        amazonS3Client.putObject(
+            PutObjectRequest(awsS3Properties.bucket, fileKey, file)
+                .withCannedAcl(CannedAccessControlList.AuthenticatedRead))
+        return amazonS3Client.getResourceUrl(awsS3Properties.bucket, fileKey)
+    }
 
 }
