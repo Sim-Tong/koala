6a10961e9d3db0c5948f736a4f95400482109633
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-08 21:05:43 +0900

    feat: UserAdapter 구현

--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/user/UserPersistenceAdapter.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/user/UserPersistenceAdapter.kt
@@ -3,20 +3,28 @@ package comit.simsimpulyi.koala.persistence.user
 import comit.simsimpulyi.koala.domain.user.model.User
 import comit.simsimpulyi.koala.domain.user.spi.QueryUserPort
 import comit.simsimpulyi.koala.domain.user.spi.SaveUserPort
+import comit.simsimpulyi.koala.persistence.user.entity.UserEntity
+import comit.simsimpulyi.koala.persistence.user.mapper.UserMapper
 import org.springframework.stereotype.Component
 import java.util.*
 
 @Component
 class UserPersistenceAdapter(
-    private val repository: UserRepository
+    private val userRepository: UserRepository,
+    private val userMapper: UserMapper
 ) : SaveUserPort, QueryUserPort {
 
     override fun saveUser(user: User): User {
-        TODO("Not yet implemented")
+        val entity = userRepository.save(userMapper.toEntity(user))
+        return userMapper.toDomain(entity)
     }
 
     override fun queryUserById(userId: UUID): User {
-        TODO("Not yet implemented")
+        return userMapper.toDomain(queryUserEntityById(userId))
+    }
+
+    fun queryUserEntityById(userId: UUID): UserEntity {
+        return userRepository.queryUserEntityById(userId)
     }
 
 }
