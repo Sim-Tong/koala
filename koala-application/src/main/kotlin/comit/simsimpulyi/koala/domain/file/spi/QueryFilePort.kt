b0cafa7b4617b0330260c0f66a61e2ba04f01ea4
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-08 18:05:14 +0900

    refactor: Port 삭제

--- koala-application/src/main/kotlin/comit/simsimpulyi/koala/domain/file/spi/QueryFilePort.kt
+++
@@ -1,8 +0,0 @@
-package comit.simsimpulyi.koala.domain.file.spi
-
-import java.util.*
-
-interface QueryFilePort {
-
-    fun queryAllFile(feedId: UUID): List<String>
-}
--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/thirdparty/aws/s3/spi/SaveFilePort.kt
+++
@@ -1,8 +0,0 @@
-package comit.simsimpulyi.koala.thirdparty.aws.s3.spi
-
-import comit.simsimpulyi.koala.persistence.file.entity.FileEntity
-
-interface SaveFilePort {
-
-    fun saveFile(file: FileEntity)
-}
