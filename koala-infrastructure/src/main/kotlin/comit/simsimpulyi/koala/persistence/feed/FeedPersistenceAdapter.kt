348b83c1dc9820f6b06bfac30059024990bf4d82
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-08 18:06:12 +0900

    feat: Feed Query 구현

--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/feed/FeedPersistenceAdapter.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/feed/FeedPersistenceAdapter.kt
@@ -3,20 +3,30 @@ package comit.simsimpulyi.koala.persistence.feed
 import comit.simsimpulyi.koala.domain.feed.model.Feed
 import comit.simsimpulyi.koala.domain.feed.spi.QueryFeedPort
 import comit.simsimpulyi.koala.domain.feed.spi.SaveFeedPort
+import comit.simsimpulyi.koala.persistence.feed.mapper.FeedMapper
+import comit.simsimpulyi.koala.persistence.file.FilePersistenceAdapter
 import org.springframework.stereotype.Component
 import java.util.*
 
 @Component
 class FeedPersistenceAdapter(
-    private val feedRepository: FeedRepository
+    private val feedRepository: FeedRepository,
+    private val filePersistenceAdapter: FilePersistenceAdapter,
+    private val feedMapper: FeedMapper
 ) : SaveFeedPort, QueryFeedPort {
 
     override fun saveFeed(feed: Feed): Feed {
-        TODO("Not yet implemented")
+        var entity = feedMapper.toEntity(feed)
+
+        entity = feedRepository.save(entity)
+        filePersistenceAdapter.saveFile(feed.filePaths, entity)
+
+        return feedMapper.toDomain(entity)
     }
 
     override fun queryFeed(feedId: UUID): Feed {
-        TODO("Not yet implemented")
+        val entity = feedRepository.queryById(feedId).orElseThrow() // exception 추가
+        return feedMapper.toDomain(entity)
     }
 
 }
--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/feed/mapper/FeedMapper.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/feed/mapper/FeedMapper.kt
@@ -14,9 +14,13 @@ abstract class FeedMapper : BaseMapper<FeedEntity, Feed> {
     @Autowired
     protected lateinit var userPersistenceAdapter: UserPersistenceAdapter
 
+    @Autowired
+    protected lateinit var filePersistenceAdapter: UserPersistenceAdapter
+
     @Mapping(target = "writerId", expression = "java(entity.getWriter().getId())")
+    @Mapping(target = "filePaths", expression = "java(filePersistenceAdapter.queryFilePath(entity.getId()))")
     abstract override fun toDomain(entity: FeedEntity): Feed
 
-    @Mapping(target = "writer", expression = "java(userPersistenceAdapter.getUser(model.getWriterId()))")
+    @Mapping(target = "writer", expression = "java(userPersistenceAdapter.queryUser(model.getWriterId()))")
     abstract override fun toEntity(model: Feed): FeedEntity
 }
