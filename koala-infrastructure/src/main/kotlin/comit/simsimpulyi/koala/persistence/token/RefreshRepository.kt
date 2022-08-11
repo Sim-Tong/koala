1be56dafe2a95ccb2f3bc1845503d5c7de3b0d81
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-11 20:36:19 +0900

    revert: 주제 X

--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/token/RefreshRepository.kt
+++
@@ -1,9 +0,0 @@
-package comit.simsimpulyi.koala.persistence.token
-
-import comit.simsimpulyi.koala.persistence.token.entity.RefreshToken
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface RefreshRepository : CrudRepository<RefreshToken, String> {
-}
