ffd5ab879a8c0267de63d7357767ea128746f71f
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-12 22:49:31 +0900

    refactor: false → true

--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/global/security/principle/AuthDetails.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/global/security/principle/AuthDetails.kt
@@ -24,18 +24,18 @@ class AuthDetails(
     }
 
     override fun isAccountNonExpired(): Boolean {
-        return false
+        return true
     }
 
     override fun isAccountNonLocked(): Boolean {
-        return false
+        return true
     }
 
     override fun isCredentialsNonExpired(): Boolean {
-        return false
+        return true
     }
 
     override fun isEnabled(): Boolean {
-        return false
+        return true
     }
 }
