2ba7fab2e36ed6d6689401fcf0c8b7f907d6af07
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-12 22:45:35 +0900

    refactor: userId → email

--- koala-application/src/main/kotlin/comit/simsimpulyi/koala/domain/user/spi/ReceiveTokenPort.kt
+++ koala-application/src/main/kotlin/comit/simsimpulyi/koala/domain/user/spi/ReceiveTokenPort.kt
@@ -1,11 +1,10 @@
 package comit.simsimpulyi.koala.domain.user.spi
 
 import comit.simsimpulyi.koala.domain.user.model.Authority
-import java.util.*
 
 interface ReceiveTokenPort {
 
-    fun generateAccessToken(userId: UUID, authority: Authority): String
+    fun generateAccessToken(email: String, authority: Authority): String
 
-    fun generateRefreshToken(userId: UUID, authority: Authority): String
+    fun generateRefreshToken(email: String, authority: Authority): String
 }
