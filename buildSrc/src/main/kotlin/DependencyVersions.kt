c6346307b9ffc62c70875662b56dc89d26224249
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-12 22:44:52 +0900

    fix: Javax Servlet Error Fix

--- buildSrc/src/main/kotlin/DependencyVersions.kt
+++ buildSrc/src/main/kotlin/DependencyVersions.kt
@@ -3,4 +3,5 @@ object DependencyVersions {
     const val MAPSTRUCT_VERSION = "1.5.2.Final"
     const val AWS_VERSION = "2.2.6.RELEASE"
     const val REDIS_VERSION = "2.7.2"
+    const val SERVLET = "4.0.1"
 }
