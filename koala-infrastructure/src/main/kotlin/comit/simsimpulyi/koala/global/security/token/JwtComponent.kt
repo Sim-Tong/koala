72364d2601a090257d94247667fe1a874d7a4df2
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-15 00:30:57 +0900

    fix: prefix 오류

--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/global/security/token/JwtComponent.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/global/security/token/JwtComponent.kt
@@ -2,7 +2,7 @@ package comit.simsimpulyi.koala.global.security.token
 
 object JwtComponent {
     const val JWT_HEADER = "Authentication"
-    const val JWT_PREFIX = "bearer "
+    const val JWT_PREFIX = "Bearer "
     const val ACCESS = "access"
     const val REFRESH = "refresh"
     const val JWT_AUTHORITY = "authority"
