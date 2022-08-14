436f7122166d70658a5edd6f96416e7d076afa78
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-14 12:43:12 +0900

    refactor: SecurityConfigurerAdapter 사용

--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/global/filter/FilterConfig.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/global/filter/FilterConfig.kt
@@ -1,15 +1,16 @@
 package comit.simsimpulyi.koala.global.filter
 
-import comit.simsimpulyi.koala.global.security.token.FilterTokenAdapter
+import comit.simsimpulyi.koala.global.security.token.JwtInterpreter
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer
+import org.springframework.security.web.DefaultSecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 class FilterConfig (
-    private val filterTokenAdapter: FilterTokenAdapter
-) : AbstractHttpConfigurer<FilterConfig, HttpSecurity>() {
+    private val filterTokenAdapter: JwtInterpreter
+) : SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
 
     override fun configure(builder: HttpSecurity) {
         builder.addFilterBefore(JwtFilter(filterTokenAdapter), UsernamePasswordAuthenticationFilter::class.java)
