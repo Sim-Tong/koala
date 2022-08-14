60dd87c770f880950e27dbd2dfc311ea850a474a
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-14 12:56:23 +0900

    refactor: userPersistenceAdapter → userRepository

--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/global/security/principle/AuthDetailsService.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/global/security/principle/AuthDetailsService.kt
@@ -1,6 +1,6 @@
 package comit.simsimpulyi.koala.global.security.principle
 
-import comit.simsimpulyi.koala.persistence.user.UserPersistenceAdapter
+import comit.simsimpulyi.koala.persistence.user.UserRepository
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
@@ -8,13 +8,13 @@ import java.util.*
 
 @Component
 class AuthDetailsService(
-    private val userPersistenceAdapter: UserPersistenceAdapter
+    private val userRepository: UserRepository
 ) : UserDetailsService {
 
     override fun loadUserByUsername(userId: String): UserDetails {
         val id = UUID.fromString(userId)
 
-        val user = userPersistenceAdapter.findUserEntityById(id)?:throw Exception() // TODO
+        val user = userRepository.queryUserEntityById(id)?:throw Exception() // TODO
         return AuthDetails(id, user.authority)
     }
 
