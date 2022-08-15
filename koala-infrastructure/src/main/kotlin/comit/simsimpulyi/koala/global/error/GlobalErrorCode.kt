5d11230524c2db884d1c66586f460a81945203d9
KimBeomJin <sdpthf@naver.com>
2022-08-15 23:12:23 +0900

    add: GlobalErrorCode

---
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/global/error/GlobalErrorCode.kt
@@ -0,0 +1,16 @@
+package comit.simsimpulyi.koala.global.error
+
+import org.springframework.http.HttpStatus
+
+enum class GlobalErrorCode(
+    private val status: HttpStatus,
+    private val message: String
+) : ErrorProperty {
+
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 입력값"),
+
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러");
+
+    override fun status(): Int = status.value()
+    override fun message(): String = message
+}
