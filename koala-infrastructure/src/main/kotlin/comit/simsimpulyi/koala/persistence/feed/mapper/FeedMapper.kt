def825dfec656e1e76198ff66757217f07d12922
KimBeomJin <sdpthf@naver.com>
2022-08-16 23:46:38 +0900

    refactor: FileRepository 바로 조회

--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/feed/mapper/FeedMapper.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/feed/mapper/FeedMapper.kt
@@ -3,10 +3,11 @@ package comit.simsimpulyi.koala.persistence.feed.mapper
 import comit.simsimpulyi.koala.domain.feed.model.Feed
 import comit.simsimpulyi.koala.persistence.BaseMapper
 import comit.simsimpulyi.koala.persistence.feed.entity.FeedEntity
-import comit.simsimpulyi.koala.persistence.file.FilePersistenceAdapter
+import comit.simsimpulyi.koala.persistence.file.FileRepository
 import comit.simsimpulyi.koala.persistence.user.UserPersistenceAdapter
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
+import org.mapstruct.Mappings
 import org.springframework.beans.factory.annotation.Autowired
 
 @Mapper
@@ -16,10 +17,12 @@ abstract class FeedMapper : BaseMapper<FeedEntity, Feed> {
     protected lateinit var userPersistenceAdapter: UserPersistenceAdapter
 
     @Autowired
-    protected lateinit var filePersistenceAdapter: FilePersistenceAdapter
+    protected lateinit var fileRepository: FileRepository
 
-    @Mapping(target = "writerId", expression = "java(entity.getWriter().getId())")
-    @Mapping(target = "filePaths", expression = "java(filePersistenceAdapter.queryFileList(entity.getId()))")
+    @Mappings(
+        Mapping(target = "writerId", expression = "java(entity.getWriter().getId())"),
+        Mapping(target = "filePaths", expression = "java(fileRepository.queryPathByFeedId(entity.getId()))")
+    )
     abstract override fun toDomain(entity: FeedEntity): Feed
 
     @Mapping(target = "writer", expression = "java(userPersistenceAdapter.queryUserEntityById(model.getWriterId()))")
--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/file/FilePersistenceAdapter.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/persistence/file/FilePersistenceAdapter.kt
@@ -3,7 +3,6 @@ package comit.simsimpulyi.koala.persistence.file
 import comit.simsimpulyi.koala.persistence.feed.entity.FeedEntity
 import comit.simsimpulyi.koala.persistence.file.entity.FileEntity
 import org.springframework.stereotype.Component
-import java.util.*
 
 @Component
 class FilePersistenceAdapter(
@@ -12,9 +11,5 @@ class FilePersistenceAdapter(
 
     fun saveFile(filePaths: List<String>, feed: FeedEntity) {
         fileRepository.saveAll(filePaths.map { FileEntity(it, feed) })
-    }
-
-    fun queryFileList(feedId: UUID): List<String> {
-        return fileRepository.queryPathByFeedId(feedId)
     }
 }
