0333fda11f22368dfec3ed1dc8070cbe3694d8fd
KimBeomJin <sdpthf@naver.com>
2022-08-16 23:36:39 +0900

    refactor: getClaims 예외 처리시 when 문 사용

--- koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/global/security/token/JwtParser.kt
+++ koala-infrastructure/src/main/kotlin/comit/simsimpulyi/koala/global/security/token/JwtParser.kt
@@ -2,7 +2,14 @@ package comit.simsimpulyi.koala.global.security.token
 
 import comit.simsimpulyi.koala.global.security.SecurityProperties
 import comit.simsimpulyi.koala.global.security.principle.AuthDetailsService
-import io.jsonwebtoken.*
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.Header
+import io.jsonwebtoken.InvalidClaimException
+import io.jsonwebtoken.Jws
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureException
+import io.jsonwebtoken.UnsupportedJwtException
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Component
@@ -14,29 +21,28 @@ class JwtParser(
 ) {
 
     private fun getClaims(token: String): Jws<Claims>? {
-        try {
-            return Jwts.parser()
+        return try {
+            Jwts.parser()
                 .setSigningKey(securityProperties.secretKey)
                 .parseClaimsJws(token)
-        } catch (e: InvalidClaimException) {
-            // TODO
-        } catch (e: ExpiredJwtException) {
-            // TODO
-        } catch (e: SignatureException) {
-            // TODO
-        } catch (e: UnsupportedJwtException) {
-            // TODO
-        } catch (e: RuntimeException) {
-            // TODO
+        } catch (e: Exception) {
+            when (e) {
+                is InvalidClaimException -> throw Exception() // TODO
+                is ExpiredJwtException -> throw Exception() // TODO
+                is SignatureException -> throw Exception() // TODO
+                is UnsupportedJwtException -> throw Exception() // TODO
+                else -> null
+            }
         }
-        return null
     }
 
-    fun getAuthentication(token: String) : Authentication? {
+    fun getAuthentication(token: String): Authentication? {
         val claims = getClaims(token)
-        
+
         claims?.let {
-            if(claims.header[Header.JWT_TYPE] != JwtComponent.ACCESS) throw Exception() // TODO
+            if (claims.header[Header.JWT_TYPE] != JwtComponent.ACCESS) {
+                throw Exception() // TODO
+            }
 
             val detail = authDetailsService.loadUserByUsername(claims.body.subject)
 
