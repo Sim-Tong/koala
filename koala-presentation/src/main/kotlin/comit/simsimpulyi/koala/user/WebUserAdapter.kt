f2192de029aea65ca6b0bc031820f23d7b59e91b
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-08-29 16:46:55 +0900

    add: Validation 추가

--- koala-presentation/src/main/kotlin/comit/simsimpulyi/koala/user/WebUserAdapter.kt
+++ koala-presentation/src/main/kotlin/comit/simsimpulyi/koala/user/WebUserAdapter.kt
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/users")
@@ -18,7 +19,7 @@ class WebUserAdapter(
 
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
-    fun signUp(request: WebSignupRequest) {
+    fun signUp(@Valid request: WebSignupRequest) {
         signupUseCase.execute(DomainSignupRequest(
             email = request.email,
             name = request.name,
--- koala-presentation/src/main/kotlin/comit/simsimpulyi/koala/user/dto/request/WebSignupRequest.kt
+++ koala-presentation/src/main/kotlin/comit/simsimpulyi/koala/user/dto/request/WebSignupRequest.kt
@@ -1,7 +1,19 @@
 package comit.simsimpulyi.koala.user.dto.request
 
+import org.hibernate.validator.constraints.Length
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+
 data class WebSignupRequest(
+
+    @field:NotNull
+    @field:Length(max = 20)
     val name: String,
+
+    @field:Email
     val email: String,
+
+    @field:NotBlank
     val password: String,
 )
